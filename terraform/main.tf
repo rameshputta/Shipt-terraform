5e46b7ab71f9da8c5726ed76803809ba2c23c8f9
rameshputta <ramhh4l@gmail.com>
2019-08-14 18:31:12 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,6 +65,11 @@ resource "aws_security_group_rule" "egress" {
     cidr_blocks     = ["0.0.0.0/0"]
 }
 # AWS redis
+resource "aws_elasticache_subnet_group" "default" {
+  name       = "${var.namespace}-cache-subnet"
+  subnet_ids = ["${aws_subnet.default.*.id}"]
+}
+
 resource "aws_elasticache_replication_group" "default" {
   replication_group_id          = "${var.cluster_id}"
   replication_group_description = "Redis cluster for Hashicorp ElastiCache example"
@@ -204,7 +209,6 @@ resource "aws_launch_configuration" "terraform-lc" {
     create_before_destroy = true
   }
 }
-
 
 resource "aws_autoscaling_group" "terraform-asg" {
   name                 = "terraform"
